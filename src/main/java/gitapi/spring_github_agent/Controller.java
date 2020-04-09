2afdc79914be06c4fefcbdc43c0e19fcbe863f78
moiiwa <moiiwa@icloud.com>
2020-04-09 19:22:30 +0300

    Now instead of arguments reponame and token passed by POST request.

--- src/main/java/gitapi/spring_github_agent/Controller.java
+++ src/main/java/gitapi/spring_github_agent/Controller.java
@@ -8,30 +8,43 @@ import gitapi.spring_github_agent.tables.Issue;
 import gitapi.spring_github_agent.tables.Issueevent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 public class Controller {
+    @Autowired
+    Connector connector;
+    @PostMapping("/github")
+    public void fetchProject(@RequestParam(name = "auth_token") String authToken, @RequestParam(name = "repo_name", required = true) String repoName) throws IOException {
+        commitsRepository.deleteAll();
+        issueRepository.deleteAll();
+        issueEventRepository.deleteAll();
+        connector.connect(authToken, repoName);
+    }
+
     @Autowired
     private CommitsRepository commitsRepository;
     @Autowired
     private IssueRepository issueRepository;
     @Autowired
     private IssueEventRepository issueEventRepository;
-    @GetMapping("/commits/all")
+    @GetMapping("/github/commits")
     public List<Commit> allCommits() {
             return commitsRepository.findAllAuthors();
     }
 
-    @GetMapping("/issues/all")
+    @GetMapping("/github/issues")
     public List<Issue> allIssues(){
         return issueRepository.findAll();
     }
 
-    @GetMapping("/issueEvents/all")
+    @GetMapping("/github/issueEvents")
     public List<Issueevent> allIssueEvents(){
         return issueEventRepository.findAll();
     }
